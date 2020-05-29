b8faa4b99673ade9c293ef1c457e7312b60d2e54
unknown <emcchos@gmail.com>
2020-05-29 19:52:43 -0300

    Leitura dos dados

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,49 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		
+		String nome;
+		char  opcao = 0;
+		double hT , vH ;
+		
+		do {
+		System.out.printf("Digite o nome do Funcionário : ");
+		nome = sc.nextLine();
+		System.out.println();
+		
+				
+		System.out.printf(" Digite as horas trabalhadas pelo funcionário: ");
+			hT = sc.nextDouble();
+			System.out.println();
+		while ( hT <= 0) {
+			System.out.printf("hora trabalhada informada é invalida.Tente novamente:  " );
+			hT = sc.nextDouble();
+			System.out.println();
+			}
+		
+		System.out.printf(" Digite o valor por cada hora trabalhada: ");
+		vH = sc.nextDouble();
+		System.out.println();
+		while ( vH <= 0) {
+			System.out.printf("Altura informada é invalida.Tente novamente:  " );
+			vH = sc.nextDouble();
+			System.out.println();
+			}
+		
+		System.out.printf(" Digitar outro atleta (S/N)? ");
+		opcao = sc.next().charAt(0);
+		System.out.println();
+		sc.nextLine();		
+		
+		}while(opcao == 'S');
+		
+		
+		
+		
+		sc.close();
+		
+		
 	}
 
 }
